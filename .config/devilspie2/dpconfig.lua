43c652d32e8aaa7b252eea686364151042a77c87
andrewbates09 <andrewbates09@gmail.com>
2014-05-15 19:58:28 -0700

    adding window transparency requirements

---
+++ .config/devilspie2/dpconfig.lua
@@ -0,0 +1,17 @@
+if (get_application_name() == "conky") then
+    set_opacity(0.85)
+end
+
+if (get_application_name() == "tint2") then
+    set_opacity(0.85)
+end
+
+if (get_application_name() == "LXTerminal") then
+    set_opacity(0.85)
+    set_window_size(100, 25)
+end
+
+if (get_application_name() == "urxvt") then
+    set_opacity(0.85)
+    set_window_size(100, 25)
+end
